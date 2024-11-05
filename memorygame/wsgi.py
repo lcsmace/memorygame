2f114c923524eb45c968c474bb2e62a3a7cfae67
lcsmace <156225128+lcsmace@users.noreply.github.com>
2024-11-05 19:14:25 -0300

    Update wsgi.py

--- memorygame/wsgi.py
+++ memorygame/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-# Adicione o caminho do projeto
+# Adicione o caminho do projeto ao sys.path
 path = Path('/home/lcsmacedo/memorygame')
 if path not in sys.path:
     sys.path.append(str(path))
@@ -10,8 +10,8 @@ if path not in sys.path:
 # Configurações do Django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'memorygame.settings'
 
-# Ativar o ambiente virtual
-activate_env = path / '/home/lcsmacedo/memorygame/venv/bin/activate'
+# Ativar o ambiente virtual (corrigido)
+activate_env = '/home/lcsmacedo/memorygame/venv/bin/activate'
 exec(open(activate_env).read(), {'__file__': activate_env})
 
 from django.core.wsgi import get_wsgi_application
