2b7d63cbe769b53d8237e3ea99d5acf401c31ed0
lcsmace <156225128+lcsmace@users.noreply.github.com>
2024-11-05 20:13:05 -0300

    Update settings.py

--- memorygame/settings.py
+++ memorygame/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#i-v1#27*r@7^60_u4fbx-6lnyjy2-z500l+4knj6ye&!c-*31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lcsmacedo.pythonanywhere.com']
 
