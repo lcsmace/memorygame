ff8a1d190dd63fa5ee7d47b2b8714596e4e62f1b
lcsmace <156225128+lcsmace@users.noreply.github.com>
2024-11-06 15:40:06 -0300

    Update urls.py

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # View que trata a URL raiz
     path('pokemon/', views.pokemon_list, name='pokemon_list'),
 ]
