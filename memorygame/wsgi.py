eb5f9aee78d7efcde3be58472e4827481015e05e
lcsmace <156225128+lcsmace@users.noreply.github.com>
2024-11-07 10:42:39 -0300

    Update wsgi.py

--- memorygame/wsgi.py
+++ memorygame/wsgi.py
@@ -1,6 +1,13 @@
+import sys
 import os
+
+# Adicione o caminho do projeto
+path = '/home/lcsmacedo/memorygame'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Defina as configurações do Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'memorygame.settings'
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memorygame.settings')
-
 application = get_wsgi_application()
